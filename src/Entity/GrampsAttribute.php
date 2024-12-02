2ab4620c4cdadb0a8895f5fb229e8d730f28537d
Gerhard Killesreiter <67070-GerhardKillesreiter@users.noreply.drupalcode.org>
2024-12-02 22:02:23 +0100

    Migration for attributes

--- src/Entity/GrampsAttribute.php
+++ src/Entity/GrampsAttribute.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Drupal\migrate_gramps\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\migrate_gramps\GrampsAttributeInterface;
 
 /**
